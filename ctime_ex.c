c20cdeee5dd62da93e8267dc34a94afeae1a6770
Tom Xue <tomxue0126@gmail.com>
2012-07-01 13:45:58 +0800

    annotation updated

--- ctime_ex.c
+++ ctime_ex.c
@@ -2,7 +2,7 @@
 #include <time.h>
 
 int main() {
-  time_t biggest = 0x7FFFFFFF;
+  time_t biggest = 0x7FFFFFFF;  // 0x80000000, the MSB is sign bit
   printf("biggest = %s \n", asctime(gmtime(&biggest)) );
   return 0;
 }
