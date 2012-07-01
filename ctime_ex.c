cc9d540477bc3708feb6d3812ce084fc1d40b188
Tom Xue <tomxue0126@gmail.com>
2012-07-01 13:41:12 +0800

    previous version: show local biggest time; current version: show UTC biggest time

--- ctime_ex.c
+++ ctime_ex.c
@@ -1,12 +1,8 @@
-// coming from "Expert C Programming"
 #include <stdio.h>
 #include <time.h>
 
-int
-main(void)
-{
- time_t biggest = 0x7FFFFFFF;
- printf("biggest = %s \n", ctime(&biggest) );
-
- return 0;
+int main() {
+  time_t biggest = 0x7FFFFFFF;
+  printf("biggest = %s \n", asctime(gmtime(&biggest)) );
+  return 0;
 }
